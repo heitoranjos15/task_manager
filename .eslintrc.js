346beb6f734873f3bf9342e462a86c7ffa3a356a
heitoranjos15 <heitoranjos96@gmail.com>
2022-10-23 11:52:07 -0300

    chore: more configs adjustments - jest configuration - eslintrc changed - more lib extensions for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  },
+  plugins: ["jest"]
+}
